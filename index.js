de61e852734e634355c454066ac355b2de43f9af
Matias Micenmacher <matamicen@gmail.com>
2021-12-30 14:32:57 -0300

    oracleTx en webserver

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 var express = require('express') //llamamos a Express
 var app = express()  
 var algosdk = require('algosdk')
+// var encodeObj = require('algosdk')
+// import algosdk, { encodeObj } from 'algosdk';
 
 
 
@@ -28,11 +30,121 @@ const test = async() => {
     }
   }
 
+  
+
   const callTx = async(signedTxn) => {
     try {
      
         const signed = decodeObj(new Uint8Array(Buffer.from(signedTxn, "base64")));
          await algodClient.sendRawTransaction(signed.blob).do();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+    // compile stateless delegate contract
+    async function compileProgram(client, programSource) {
+      let encoder = new TextEncoder();
+      let programBytes = encoder.encode(programSource);
+      let compileResponse = await client.compile(programBytes).do();
+      return compileResponse;
+  }
+
+
+  const oracleTx = async() => {
+    try {
+
+
+      let oracleTemplate = `
+#pragma version 3
+txn TypeEnum
+int pay
+==
+global GroupSize
+int 3
+==
+&&
+txn Receiver
+addr YEUJW5EPVUDGXYG67LWCL376GMHYKORJECSB2JAW5WY4ESL3CEHPRSEWX4
+==
+&&
+txn CloseRemainderTo
+global ZeroAddress
+==
+&&
+txn RekeyTo
+global ZeroAddress
+==
+&&
+txn Fee
+int 1000
+<=
+&&
+txn Note
+btoi
+int <exchange_rate>
+==
+&&
+txn LastValid
+int <lastvalid>
+<=
+&&
+return
+`;
+
+        const algodToken = '';
+        const algodServer = "https://api.testnet.algoexplorer.io";
+        const algodPort = '';
+        algodClient = new algosdk.Algodv2(algodToken, algodServer,algodPort);
+
+        const status = (await algodClient.status().do());
+        if (status === undefined) {
+            throw new Error("Unable to get node status");
+        }
+        // obtengo last round + 10
+        console.log('lastRound: '+status["last-round"])  
+        lastvalid = status["last-round"] + 10
+
+        // armo el logicsig del oracle
+
+        oracleTemplate = oracleTemplate.replace("<exchange_rate>", parseInt("00025000"));    
+        let program = oracleTemplate.replace("<lastvalid>", lastvalid); 
+        console.log(program)
+        compilation = await compileProgram(algodClient, program);
+        //generate unique filename
+        // let uintAr = _base64ToArrayBuffer(compilation.result);
+        console.log(compilation.result)
+        oracleProgramReferenceProgramBytesReplace = Buffer.from(compilation.result, "base64");
+        console.log(oracleProgramReferenceProgramBytesReplace)
+        program_array = new Uint8Array (oracleProgramReferenceProgramBytesReplace);
+        args = null;
+        // let lsig = algosdk.makeLogicSig(program_array, args);
+        oracle_lsig = new algosdk.LogicSigAccount(program_array, args);
+        console.log('Oracle_logicsic_account: '+oracle_lsig.address())
+
+        let oracle_sk = algosdk.mnemonicToSecretKey("popular sauce pride off fluid you come coffee display list stadium blood scout bargain segment laptop hand employ demise grass sign adult want abstract exhibit")
+        console.log("oracle Address:" +oracle_sk.addr.toString())
+        console.log(oracle_lsig)
+        oracle_lsig.sign(oracle_sk.sk);
+        console.log(oracle_lsig)
+        console.log('oracle_sk.sk')
+        console.log(oracle_sk)
+        console.log(oracle_sk.sk)
+        console.log('Oracle_logicsic_account: '+oracle_lsig.address())
+        // pepe = oracle_lsig.get_obj_for_encoding()
+        // console.log(pepe)
+        // const aux = Buffer.from(algosdk.encodeObj(oracle_lsig)).toString("base64");
+        const encoded1 = Buffer.from(algosdk.encodeObj(oracle_lsig)).toString("base64");
+
+        // encoded1 = algosdk.encodeObj(aux)
+        // console.log(encoded1)
+
+
+
+        return encoded1
+        // + encoded1
+        // const signed = decodeObj(new Uint8Array(Buffer.from(signedTxn, "base64")));
+        //  await algodClient.sendRawTransaction(signed.blob).do();
     } catch (err) {
       console.error(err);
     }
@@ -64,6 +176,14 @@ console.log('llego llamado!' )
   callTx(req.body.param1)
   res.json({mensaje: req.body.param1})  
 })
+
+app.post('/oracle', async function(req, res) {
+  //   res.json({ mensaje: 'Método post' })  
+  console.log('llama oracle!' )
+  
+    envioSignedTx = await oracleTx()
+    res.json({signedOracle: envioSignedTx})  
+  })
 
 app.del('/', function(req, res) {
   res.json({ mensaje: 'Método delete' })  
